18a949a02a35d314c4ac88394cb109c5074ecde3
Simphiwe-Nhlabati <thomasnhlabati770@gmail.com>
2025-05-29 19:40:04 +0200

    Add files via upload

--- sticky_notes/notes/tests.py
+++ sticky_notes/notes/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from .models import Notes
+from django.urls import reverse
 # Create your tests here.
 
 
-class TestNotes(TestCase):
+class TestModelNotes(TestCase):
   
     """
     Create title and 
@@ -23,4 +24,37 @@ class TestNotes(TestCase):
         self.assertEqual(note.body, 'This is my lil bro so bright')
         
         
+class TestViewsNotes(TestCase):
+    
+    def setUp(self):
+        Notes.objects.create(title='hello lil bro howzit',
+                             body='This is my lil bro so bright')
         
+    def test_read_sticky_notes(self):
+        response = self.client.get(reverse('read_sticky_notes'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.note.title, 'hello lil bro howzit')
+        self.assertEqual(self.note.body, 'This is my lil bro so bright')
+        
+    def test_create_sticky_notes(self):
+        response = self.client.post(reverse('create_sticky_notes'), {
+            'title': 'hello lil bro howzit',
+            'body': 'This is my lil bro so bright.'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Notes.objects.filter(title='hello lil bro howzit'))
+        
+    def test_update_sticky_notes(self):
+        response = self.client.post(reverse('update_sticky_notes', args=[1]), {
+            'title': 'Hello big bro howzit',
+            'body': 'This is my big bro so shiny.'
+        })
+        self.assertEqual(response.status_code, 302)
+        note = Notes.objects.get(id=1)
+        self.assertEqual(note.title, 'Hello big bro howzit')
+        self.assertEqual(note.body, 'This is my big bro so shiny')
+        
+    def test_delete_sticky_notes(self):
+        response = self.client.post(reverse('delete_sticky_notes', args=[1]))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Notes.objects.filter(id=1))
